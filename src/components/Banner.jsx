f27d649c64cb9b5ea3647a6eefd76d8bb14998e4
Raisul-0007 <raisulislamriat007@gmail.com>
2026-08-27 23:06:02 +0600

    Banner Start

---
+++ src/components/Banner.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Container from './Container'
+
+const Banner = () => {
+  return (
+    <div>
+        <Container>
+            Banner
+        </Container>
+    </div>
+  )
+}
+
+export default Banner
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navber from '../components/Navber'
+import Banner from './../components/Banner';
 
 const Home = () => {
   return (
     <div>
       <Navber/>
+      <Banner/>
     </div>
   )
 }
