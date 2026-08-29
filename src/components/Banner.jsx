ec746f8c3b1ce63955f4d12e3ffe81e9d3b52708
Raisul-0007 <raisulislamriat007@gmail.com>
2026-08-29 22:45:09 +0600

    banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,12 +1,34 @@
-import React from 'react'
 import Container from './Container'
-
+import React, { useRef, useState } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import {Autoplay, Navigation } from 'swiper/modules';
+import one from "../assets/one.jpg"
+import two from "../assets/two.jpg"
+import three from "../assets/three.jpg"
 const Banner = () => {
   return (
     <div>
-        <Container>
-            Banner
-        </Container>
+            <Swiper  spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        navigation={true}
+        modules={[Autoplay, Navigation]}
+        className="mySwiper">
+        <SwiperSlide>
+          <img className='w-full h-180' src={one} alt="one" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img className='w-full h-180' src={two} alt="two" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img className='w-full h-180' src={three} alt="three" />
+        </SwiperSlide>
+      </Swiper>
     </div>
   )
 }
