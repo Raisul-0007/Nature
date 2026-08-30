6ccb0ff4edbf041cd1727041ce9b09a651489a36
Raisul-0007 <raisulislamriat007@gmail.com>
2026-08-31 00:14:26 +0600

    Api distribute

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
+import ApiProvider from "./components/ApiData.jsx"
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ApiProvider>
+      <App/>
+    </ApiProvider>
   </StrictMode>,
 )
