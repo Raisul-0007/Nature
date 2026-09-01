16d3e090f8273d5a369285ca3fb65a5554952c54
Raisul-0007 <raisulislamriat007@gmail.com>
2026-09-01 22:43:55 +0600

    card New

--- src/components/ApiData.jsx
+++ src/components/ApiData.jsx
@@ -1,27 +1,37 @@
-import axios from 'axios'
-import React, { createContext, useEffect, useState } from 'react'
+import axios from "axios";
+import React, { createContext, useEffect, useState } from "react";
 
-export const ApiContext = createContext()
-const ApiData = ({children}) => {
-    const [data, setData] = useState([])
-    const [loading, setLoading] = useState(true)
-    const Apifetch = async()=>{
-        try{
-            setLoading(true)
-            const response = await axios.get("https://dummyjson.com/products?&limit=0")
-            setData(response.data.products)
-        }finally{
-            setLoading(false)
-        }
+export const ApiContext = createContext();
+
+const ApiData = ({ children }) => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const ApiFetch = async () => {
+    try {
+      setLoading(true);
+
+      const response = await axios.get(
+        "https://dummyjson.com/products?limit=0"
+      );
+
+      setData(response.data.products);
+    } catch (error) {
+      console.log("API Error", error);
+    } finally {
+      setLoading(false);
     }
-    useEffect(()=>{
-        Apifetch()
-    },[])
-  return (
-    <ApiContext.Provider value={{data, loading}}>
-        {children}
-    </ApiContext.Provider>
-  )
-}
+  };
 
-export default ApiData
+  useEffect(() => {
+    ApiFetch();
+  }, []);
+
+  return (
+    <ApiContext.Provider value={{ data, loading }}>
+      {children}
+    </ApiContext.Provider>
+  );
+};
+
+export default ApiData;
--- src/components/New.jsx
+++ src/components/New.jsx
@@ -1,13 +1,28 @@
 import React, { useContext } from 'react'
 import { ApiContext } from './ApiData';
-
+import Container from './Container'
 const New = () => {
     let {data} = useContext(ApiContext)
   return (
     <div>
+      <Container clasName="">
+        <div className="text-5xl font-semibold py-10">
+          <h2>New Collection</h2>
+        </div>
+        <div className="flex flex-wrap gap-5">
       {data.map((item)=>(
-        <h2>{item.title}</h2>
+        <div className="w-1/4">
+          <div className="bg-[rgba(0,253,0,0.11)] py-8 rounded-t-4xl">
+            <img src={item.thumbnail} alt="" />
+          </div>
+          <div className="flex justify-between items-end  bg-[#33FF33] px-5 py-4 rounded-b-4xl">
+            <h2 className='text-3xl capitalize'>{item.title.slice(0,13)}</h2>
+            <p className='text-xl'>{item.price}</p>
+            </div> 
+        </div>
       ))}
+      </div>
+      </Container>
     </div>
   )
 }
