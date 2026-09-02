08752056e7b3cfe7226a8fb146e895aefd344890
Raisul-0007 <raisulislamriat007@gmail.com>
2026-09-02 20:58:59 +0600

    New complete

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,5 +1,5 @@
 import Container from './Container'
-import React, { useRef, useState } from 'react';
+import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/navigation';
--- src/components/New.jsx
+++ src/components/New.jsx
@@ -1,27 +1,47 @@
 import React, { useContext } from 'react'
 import { ApiContext } from './ApiData';
 import Container from './Container'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Autoplay, Pagination } from 'swiper/modules';
 const New = () => {
     let {data} = useContext(ApiContext)
   return (
     <div>
       <Container clasName="">
-        <div className="text-5xl font-semibold py-10">
-          <h2>New Collection</h2>
+        <div className=" py-10">
+          <h2 className='text-5xl font-semibold text-center text-primary'>New Collection</h2>
         </div>
+        <Swiper
+        slidesPerView={4}
+        spaceBetween={10}
+        modules={[ Pagination]}
+        autoplay={{
+          delay: 2000,
+          disableOnInteraction: false,
+        }}
+        className="mySwiper"
+      >
         <div className="flex flex-wrap gap-5">
       {data.map((item)=>(
-        <div className="w-1/4">
+        <SwiperSlide className="w-1/4">
           <div className="bg-[rgba(0,253,0,0.11)] py-8 rounded-t-4xl">
             <img src={item.thumbnail} alt="" />
           </div>
-          <div className="flex justify-between items-end  bg-[#33FF33] px-5 py-4 rounded-b-4xl">
-            <h2 className='text-3xl capitalize'>{item.title.slice(0,13)}</h2>
-            <p className='text-xl'>{item.price}</p>
+          <div className=" rounded-b-4xl">
+            <div className="flex justify-between bg-primary items-end px-5 pt-6 pb-4">
+              <h2 className='text-3xl capitalize'>{item.title.slice(0,10)}...</h2>
+            <p className='text-xl text-blue-800'>${(item.price-((item.price * item.discountPercentage) / 100)).toFixed(2)}</p>
+            </div>
+            <div className="rounded-b-4xl text-center py-4 cursor-pointer hover:bg-[rgba(0,253,0,0.46)] hover:text-secondary bg-white border border-primary">
+              Add To cart
+            </div>
             </div> 
-        </div>
+        </SwiperSlide>
       ))}
       </div>
+      </Swiper>
       </Container>
     </div>
   )
