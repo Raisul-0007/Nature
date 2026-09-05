9489931d0ecff96bc580849229de2ea6b57b78bd
Raisul-0007 <raisulislamriat007@gmail.com>
2026-09-05 19:36:47 +0600

    Footer component create

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navber from '../components/Navber'
 import Banner from './../components/Banner';
 import New from '../components/New';
+import Footer from '../components/Footer';
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Navber/>
       <Banner/>
       <New/>
+      <Footer/>
     </div>
   )
 }
